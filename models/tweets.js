792f9e782282dc41ea1d7dc9730ad81e1861314d
bunn1 <nicjohbun@gmail.com>
2022-11-30 08:33:14 +0100

    unmark text

--- models/tweets.js
+++ models/tweets.js
@@ -1,18 +1,18 @@
-// import Schema from 'validate';
+import Schema from 'validate';
 
-// const postSchema = new Schema({
-//     status: {
-//         type: 'String',
-//         default: 'public',
-//         enum: ['public', 'private']
-//     },
-//     username: {
-//         type: 'String',
-//         required: 'true'
-//     }
-// })
+const postSchema = new Schema({
+    status: {
+        type: 'String',
+        default: 'public',
+        enum: ['public', 'private']
+    },
+    username: {
+        type: 'String',
+        required: 'true'
+    }
+})
 
-// export { postSchema };
+export { postSchema };
 
 
 
