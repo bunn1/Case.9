49a56c8e23aace845b6d25c40b8862dcecc11cc2
bunn1 <nicjohbun@gmail.com>
2023-02-26 11:19:42 +0100

    Comments

--- routes/route-edit.js
+++ routes/route-edit.js
@@ -6,6 +6,7 @@ import { connectDatabase } from '../configurations/mongodb.js';
 
 const router = express.Router();
 
+// Hämtar tweet från databasen baserat på Id. Den ansluter till databasen och sen hämtar den tweeten med hjälp av findOne()
 export async function getTweetById(id) {
     try {
         const db = await connectDatabase();
@@ -17,6 +18,7 @@ export async function getTweetById(id) {
     }
   }
 
+  // Funktionen uppdaterar text fältet av en tweet med rätt id i databasen
    export async function updateTweetById(id, text) {
     try {
       const db = await connectDatabase();
@@ -27,6 +29,7 @@ export async function getTweetById(id) {
     }
   }
 
+  // GET rutt skapas för att hämta tweet baserat på id och sedan visas en vy för att redigera tweeten.
   router.get('/tweets/:id/edit', async (req, res) => {
     try {
       const tweet = await getTweetById(req.params.id);
@@ -37,7 +40,7 @@ export async function getTweetById(id) {
     }
   });
 
-  
+  // Routen triggas när en användare skickar in en edit form för en tweet med specifikt ID
   router.post('/tweets/:id/edit', async (req, res) => {
     try {
       await updateTweetById(req.params.id, req.body.text);
